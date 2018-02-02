effe06fc106c8b779d589a4347b6efc040e0f947
inaba.sho <inaba.sho@donuts.ne.jp>
2018-02-02 17:04:00 +0900

    commit

---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Test : MonoBehaviour
+{
+	void Start ()
+    {
+        HotCold();
+	}
+
+    /// <summary>
+    /// Coldの場合はストリーム(Observable)が動いてない
+    /// Hotにする事でSubscribeする前に動く
+    /// </summary>
+    private void HotCold()
+    {
+        var subject = new Subject<string>();
+        var sourceObservable = subject.AsObservable();
+        var stringObservable = sourceObservable.Scan((p, c) => {
+            //Debug.Log("p:" + p);
+            //Debug.Log("c:" + c);
+            return p + c;
+        });//.Share(); // Publish(); // subjectのObservable(登録)を返す
+
+        // stringObservable.Connect();
+
+        subject.OnNext("A");
+        subject.OnNext("B");
+
+        stringObservable.Subscribe(msg => Debug.Log("msg:" + msg));
+
+        subject.OnNext("C");
+        subject.OnNext("D");
+
+        subject.OnCompleted();
+    }
+}
